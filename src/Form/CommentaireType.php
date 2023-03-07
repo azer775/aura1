3bacf4adc08e992b11fd83a1f61b5bc37d5f1a6b
azer775 <80197483+azer775@users.noreply.github.com>
2023-03-08 00:01:41 +0100

    Add files via upload

--- src/Form/CommentaireType.php
+++ src/Form/CommentaireType.php
@@ -6,20 +6,17 @@ use App\Entity\Commentaire;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+
 class CommentaireType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('text', TextType::class, [
-                'attr' => ['class' => 'form-control']
-            ])
-            ->add('date', TextType::class, [
-                'attr' => ['class' => 'form-control']
-            ])
-            ->add('post')
-            ->add('membre')
+            ->add('text')
+           // ->add('membre')
+            //->add('date')
+            //->add('post')
+            //->add('membre')
         ;
     }
 
